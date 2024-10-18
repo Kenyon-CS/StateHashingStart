71d653c409b186c249cc2ff1afc78307342af7df
James Skön <skonjp@kenyon.edu>
2024-10-18 12:17:00 -0400

    Create stateData.cpp

---
+++ stateData.cpp
@@ -0,0 +1,8 @@
+#include "stateData.h"
+#include <iomanip>
+
+// Constructor
+stateData::stateData(string nm, string cap, double g, string mot, string fl, double ar)
+    : name(nm), capital(cap), gdp(g), motto(mot), flower(fl), area(ar) {}
+
+// The rest of the code ...
