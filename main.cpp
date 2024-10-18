ed0608e2c900acc5e234bc43eced12b692bad4d6
James Skön <skonjp@kenyon.edu>
2024-10-18 12:17:56 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "stateData.h"
+#include "hashT.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    hashT hashTable;
+
+    stateData california("California", "Sacramento", 3200.5, "Eureka", "Golden Poppy", 163696);
+
+    // More code.
+  
+    return 0;
+}
